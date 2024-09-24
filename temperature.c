e1f3e81ce960822fb3ba3038f71aac39e74b23ea
Jenil060906 <viradiajenil@gmail.com>
2024-09-24 18:48:58 +0530

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,12 @@
+//To convert degree celsius to degree farenheit.
+#include<stdio.h>
+void main(){
+//Taking c as the value of user input degree celsius number.
+     float c;
+     printf("Enter the value of temperature in degree celcius:");
+     scanf("%f",&c);
+  //By using the below formula we can convert the value of degree celsius to degree farenheit.
+     float f=((9*c)/5)+32;
+     printf("The value of provided temperature in degree farenheit is: %f",f);
+printf("\nThis program is created by Jenil Viradia. ")
+}
