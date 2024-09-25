59b24ee50292a374f3978e629b00aa25c3660781
Jenil060906 <viradiajenil@gmail.com>
2024-09-25 18:51:54 +0530

    Add files via upload

---
+++ simpleinterest.c
@@ -0,0 +1,12 @@
+//Calculating the simple interest by given values of principle,rate and time.
+#include<stdio.h>
+void main(){
+//Here all the values are already being provided so we will just apply the related datatypes to the given values.
+       int p=50000;
+       float r=5.5;
+       int t=3;
+//Calculating the simple interest by using the below formula.
+       float s=(p*r*t)/100;
+       printf("The value of simple interest is: %f",s);
+printf("This program is created by Jenil Viradia.");
+}
