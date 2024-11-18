4a24b8493f335a5eed9975c8292841baa59a40c3
Jenil060906 <viradiajenil@gmail.com>
2024-11-18 21:31:09 +0530

    Add files via upload

---
+++ Prac10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    //USING DECREMENT FOR DECREASING TIME IN SECONDS.
+    for (i = 10; i >= 0; i--)
+    {
+        printf("%d\n", i);
+    }
+    printf("COUNTDOWN COMPLETED!");
+    printf("THIS CODE IS PREPARED BY 24CE141_JENIL VIRADIA.");
+    return 0;
+}
