a176f209e4c83d24d25bdeb59d6fb69251e12b59
Jenil060906 <viradiajenil@gmail.com>
2024-11-18 20:56:01 +0530

    Delete name.c

--- name.c
+++
@@ -1,6 +0,0 @@
-//Using stdio for the standard input and output program.
-#include<stdio.h>
-void main(){
-//My first C program by using the printf functions.
-   printf("\nThis program is created by Jenil Viradia.");
-}
