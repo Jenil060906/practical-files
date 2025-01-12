77f74dae39be65a63d73eb64399e08684b9e1707
Jenil060906 <viradiajenil@gmail.com>
2025-01-12 22:09:37 +0530

    Add files via upload

---
+++ prac1.c
@@ -0,0 +1,9 @@
+// ENTERING THE PRE-PROCESSOR OF C FILE.
+#include <stdio.h>
+// DEFINING PRE-DEFINED MAIN FUNCTION.
+int main()
+{
+    printf("JENIL.");
+    printf("\nTHIS PROGRAM IS CREATED BY 24CE141_JENIL VIRADIA.");
+    return 0;
+}
